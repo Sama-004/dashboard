f9a120feb5ecdc5b88b5a59224b036e020ecc3ba
Sama-004 <70210929+Sama-004@users.noreply.github.com>
2024-04-26 08:18:54 +0530

    added table

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,29 +1,39 @@
 import { tableData } from "../data/table";
 export default function Table() {
   return (
-    <div className="overflow-x-auto">
-      <table className="bg-white">
-        <thread className="border">
-          <tr>
-            <th>Order ID</th>
-            <th>Status</th>
-            <th>Transaction ID</th>
-            <th>Refund Date</th>
-            <th>Order Amount</th>
-          </tr>
-          <tbody>
-            {tableData.map((row, orderId) => (
-              <tr key={orderId}>
-                <td className="text-blue-800">{row.orderId}</td>
-                <td className="text-gray-600">{row.status}</td>
-                <td className="text-gray-600">{row.transactionId}</td>
-                <td className="text-gray-600">{row.refundDate}</td>
-                <td className="text-gray-600">{row.orderAmount}</td>
-              </tr>
-            ))}
-          </tbody>
-        </thread>
-      </table>
+    <div>
+      <div className="bg-white p-3 rounded-md">
+        <div className="grid grid-cols-5 my-3 bg-[#F2F2F2] rounded-sm p-3 text-sm font-medium text-[#4D4D4D]">
+          <div>Order Id</div>
+          <div>Status</div>
+          <div>Transatction Id</div>
+          <div>Refund Date</div>
+          <div className="place-self-end">Order Amount</div>
+        </div>
+        <div className="border-b-2">
+          {tableData.map((row, orderId) => (
+            <div
+              key={orderId}
+              className="grid grid-cols-5 my-3 rounded-sm px-3 text-sm font-medium text-[#4D4D4D]">
+              <div className="text-[#146EB4]">{row.orderId}</div>
+              <div className="flex items-center">
+                <div
+                  className={`w-2.5 h-2.5 rounded-full ${
+                    row.status === "Successfull"
+                      ? "bg-green-400"
+                      : row.status === "Failed"
+                      ? "bg-red-400"
+                      : "bg-gray-400"
+                  }`}></div>
+                <div className="pl-1">{row.status}</div>
+              </div>
+              <div>{row.transactionId}</div>
+              <div>{row.refundDate}</div>
+              <div className="place-self-end">{row.orderAmount}</div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
